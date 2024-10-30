920fad9e88955d1b5d769960b09c2f11e061c985
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2024-10-31 00:54:22 +0100

    Specials added

--- src/pages/home/Specials.jsx
+++ src/pages/home/Specials.jsx
@@ -1,8 +1,59 @@
+import { faMotorcycle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import { meals } from "./specialsData";
 import "./styles/specials.css";
+
 export default function Specials() {
 	return (
-		<div>
-			<h1>Specials goes here</h1>
-		</div>
+		<section className="container specials-grid">
+			<div className="specials-header">
+				<h2>This weeks specials!</h2>
+				<Link to="/order-online" className="btn">
+					Online Menu
+				</Link>
+			</div>
+			{meals.map((item, index) => {
+				return (
+					<article className="item-card" key={index}>
+						<div className="item-card-image">
+							<img src={item.image} alt={item.name} />
+						</div>
+						<div className="item-card-header">
+							<h3>{item.name}</h3>
+							<span>{item.price}</span>
+						</div>
+						<div className="item-card-footer">
+							<p>{item.description}</p>
+							<Link to="">
+								Order a delivery{" "}
+								<span>
+									<FontAwesomeIcon icon={faMotorcycle} />
+								</span>
+							</Link>
+						</div>
+					</article>
+				);
+			})}
+		</section>
 	);
 }
+
+/* 
+
+<article className="item-card">
+				<div className="item-card-image">
+					<img src={item.image} alt={item.name} />
+				</div>
+				<div className="item-card-header">
+					<h3>{item.name}</h3>
+					<span>{item.price}</span>
+				</div>
+				<div className="item-card-footer">
+					<p>{item.description}</p>
+					<Link to="">
+						Order a delivery <FontAwesomeIcon icon={faMotorcycle} />
+					</Link>
+				</div>
+			</article>
+*/
---
+++ src/pages/home/specialsData.js
@@ -0,0 +1,25 @@
+import tempImage from "/images/hero-image.jpg";
+export const meals = [
+	{
+		name: "Greek Salad",
+		image: tempImage,
+		price: "$12.99",
+		description: `The famous greek salad of crispy lettuce, peppers, olives and 
+		  our Chicago style feta cheese, garnished with crunchy garlic and rosemary 
+		  croutons.`,
+	},
+	{
+		name: "Bruschetta",
+		image: tempImage,
+		price: "$5.99",
+		description: `Our Bruschetta is made from grilled bread that has been 
+		  smeared with garlic and seasoned with salt and olive oil.`,
+	},
+	{
+		name: "Lemon Dessert",
+		image: tempImage,
+		price: "$5.00",
+		description: `This comes straight from grandma's recipe book, every last 
+		  ingredient has been sourced and is as authentic as can be imagined.`,
+	},
+];
